a59644559211a194663f0cffa7643bc05e7e015b
OrFlorentin <orflorentin23@gmail.com>
2024-06-15 18:09:28 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa did some thing with package json idk man the front works for some reason

--- backend/config.py
+++ backend/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    microsoft_login_redirect_uri: str
-    environment: str
+    microsoft_login_redirect_uri: str = ""
+    environment: str = "development"
 
 
 settings = Settings()
